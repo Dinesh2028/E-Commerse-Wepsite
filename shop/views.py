9a0eeef4212c5c4372335aa087c3f5867aa4f06e
Dinesh Kumar <dineshkumar20072002@gmail.com>
2025-10-12 19:02:36 +0530

    fix images

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import *
+from .models import Catagory
 
 # Create your views here.
-def home(reqest):
-    return render(reqest,"shop/index.html")
+def home(request):
+    return render(request,"shop/index.html")
 
-def register(reqest):
-    return render(reqest,"shop/register.html")
+def register(request):
+    return render(request,"shop/register.html")
+
+def collections(request):
+    categories = Catagory.objects.filter(status=0).prefetch_related('product_set')
+    return render(request,"shop/collections.html",{"categories": categories})
